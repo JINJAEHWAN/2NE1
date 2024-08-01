ac85513a3f67690c4138fec8ef9191c97bb9e4d6
Robbie <robbie@>
2024-08-01 10:48:35 +0900

    dev 추가

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -16,6 +16,8 @@ import java.util.Scanner;
  * 프로젝트 구조를 변경하거나 기능을 추가해도 괜찮습니다!
  * 구현에 도움을 주기위한 Base 프로젝트입니다. 자유롭게 이용해주세요!
  */
+
+//git branch dev
 public class CampManagementApplication {
     // 데이터 저장소
     private static List<Student> studentStore;
